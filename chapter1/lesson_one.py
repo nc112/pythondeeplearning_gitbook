0a39f39fe62080b63d73d1166910a26cf87e71ed
nicolas <nc112_85@163.com>
2019-04-30 21:16:05 +0800

    Updateforchapter2

--- chapter1/lesson_one.py
+++ chapter1/lesson_one.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.image import imread
 
 class DateGenerate:
     def __init__(self):
@@ -7,26 +8,39 @@ class DateGenerate:
         self.startnumber = 0
         self.stopnumber = 6
     def generateSin(self, start=None, stop=None, step=None):
-        x=[]
-        y=[]
+        x = []
+        ysin = []
+        ycos = []
         if None == start or\
             None == stop or\
             None == step:
             x = np.arange(self.startnumber, self.stopnumber, self.step)
-            y = np.sin(x)
+            ysin = np.sin(x)
+            ycos = np.cos(x)
         else:
             x = np.arange(start, stop, step)
-        return x,y
+        return x,ysin,ycos
 
 class PlotArray:
     def __init__(self):
         pass
-    def PlotandShow(self, x=None, y=None):
-        plt.plot(x, y)
-        plt.show()
+    def PlotChart(self, x=None, ysin=None, ycos=None):
+        #Title
+        plt.title('Sin&Cos Chart')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+
+        plt.plot(x, ysin, linestyle='--', label='Sin')
+        plt.plot(x, ycos, label='Cos')
+        plt.legend()
 
 dg = DateGenerate()
 dginput = dg.generateSin()
 
 pa = PlotArray()
-pa.PlotandShow(dginput[0], dginput[1])
+#pa.PlotChart(dginput[0], dginput[1], dginput[2])
+
+#input png image
+img = imread('lena.png')
+plt.imshow(img)
+plt.show()
