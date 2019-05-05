e89e87a974e4a740a5b4cd4a9f4be05c262d9a4c
nicolas <nc112_85@163.com>
2019-05-05 19:25:41 +0800

    Update for day8

---
+++ day8/day8_chapter5.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class add_process:
+    def __init__(self):
+        self.a = 0
+        self.b = 0
+        self.c = 0
+    def forward
+class mul_process:
+    def __init__(self):
