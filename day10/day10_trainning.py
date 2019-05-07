8419f5be3564a4d24bd9ef71d5114ea69bc4bf10
nicolas <nc112_85@163.com>
2019-05-07 19:43:44 +0800

    Update for day8

--- day10/day10_trainning.py
+++ day10/day10_trainning.py
@@ -6,5 +6,27 @@ ipsize = 784 #used for MNIST
 hlist = [100, 100, 100, 100, 100, 100]
 opsize = 10
 
-#
+class SGD:
+    def __init__(self, lrate = 0.01):
+        self.lrate = lrate
+    '''
+    parameter is a dictionary,
+    grads is a result array, shall be calculated before call the function
+    grads[] contain dl/dp
+    '''
+    def updatenetwork(self, parameters, grads):
+        for key in parameters.keys():
+            parameters -= self.lrate * grads[key]
+
+#Initilize network
 network = MultiLayerNet(input_size=ipsize, hidden_size_list=hlist, output_size=opsize)
+optimizer = SGD()
+
+'''
+Define neural network parameters
+'''
+max_epochs = 200
+train_size = 100
+batch_size = 100
+
+
