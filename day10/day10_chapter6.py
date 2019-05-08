eafccfba8c214e03f9821acddf414d5552ed0f98
nicolas <nc112_85@163.com>
2019-05-08 14:07:49 +0800

    Update for day8

--- day10/day10_chapter6.py
+++ day10/day10_chapter6.py
@@ -1,13 +1,115 @@
 import numpy as np
+from common.multi_layer_net import MultiLayerNet
+from common.multi_layer_net_extend import MultiLayerNetExtend
+from common.trainer import Trainer
 from dataset.mnist import load_mnist
+import matplotlib.pyplot as plt
 
-'''
-return value:
-(a,b), (c,d): 
-a: train image
-b: train label
-C: test image
-d: test label
-'''
+class DrawFigure:
+    def __init__(self, title):
+        plt.title(title)
+        self.fig = plt.figure(1)
+    def setXrange(self,xmin=None,xmax=None):
+        if None == xmin or None == xmax:
+            pass
+        else:
+            plt.xlim(xmin, xmax)
+    def setYrange(self,ymin=None,ymax=None):
+        if None == ymin or None == ymax:
+            pass
+        else:
+            plt.ylim(ymin, ymax)
+    def plotchart(self,x,y):
+        plt.plot(x,y)
+    def plotshow(self):
+        plt.show()
+
+
+def drawchart(title, ylist):
+    df = DrawFigure(title)
+    epoch = np.arange(0, epoch_count, 1)
+    for item in ylist:
+        df.plotchart(epoch, item)
+    df.plotshow()
+
+
+class Dropout:
+    def __init__(self, dropout_ratio=0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+
+    '''
+    * convert paras to tuple
+    ** convert params to dictionary
+    '''
+    def forward(self, x, train_flg=True):
+        if train_flg:
+
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else:
+            return x * (1.0 - self.dropout_ratio)
+
+    def backward(self, dout):
+        return dout * self.mask
+
+class SGD:
+    def __init__(self, lrate=0.01):
+        self.lrate = lrate
+
+    '''
+    parameter is a dictionary,
+    grads is a result array, shall be calculated before call the function
+    grads[] contain dl/dp
+    '''
+
+    def update_network(self, parameters, grads):
+        for key in parameters.keys():
+            parameters[key] -= self.lrate * grads[key]
+
+# Pre definition
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
+#Validation for over fitting
+x_train = x_train[:300]
+t_train = t_train[:300]
 
+ipsize = 784  # used for MNIST
+hlist = [100, 100, 100, 100, 100, 100]
+opsize = 10
+use_dropoutFlag = True
+dp_ration = 0.15
+
+max_epochs = 201
+train_size = x_train.shape[0]
+batch_size = 100
+#trainning_loop = int(1e9)
+
+train_loss_list = []
+train_accurate_list = []
+test_accurate_list = []
+
+'''
+#Define neural network parameters
+{
+    max_epochs: trainning number
+    train_size: size of the train data
+    batch_size: size of the batch
+}
+iter_per_epoch: complete one trainning
+'''
+iter_per_epoch = max(train_size/batch_size, 1)
+epoch_count = 301
+
+#Initilize network
+#network = MultiLayerNet(input_size=ipsize, hidden_size_list=hlist, output_size=opsize)
+network = MultiLayerNetExtend(input_size=ipsize, hidden_size_list=hlist, output_size=opsize,
+                              use_dropout=use_dropoutFlag, dropout_ration=dp_ration)
+optimizer = SGD()
+trainer = Trainer(network, x_train, t_train, x_test, t_test,
+                  epochs=epoch_count, mini_batch_size=100,
+                  optimizer='sgd', optimizer_param={'lr': 0.01}, verbose=True)
+trainer.train()
+train_accurate_list, test_accurate_list = trainer.train_acc_list, trainer.test_acc_list
+
+#Valitation
+drawchart('Optimize network by Dropout', [train_accurate_list, test_accurate_list])
--- day10/day10_trainning.py
+++ day10/day10_trainning.py
@@ -1,101 +1,34 @@
 import numpy as np
-from common.multi_layer_net import MultiLayerNet
-from dataset.mnist import load_mnist
-import matplotlib.pyplot as plt
+class MyTrainer:
+    def __init__(self, networktype, trainning_loop, train_size, batch_size, x_train, t_train, optimizer='SGD'):
+        self.networktype =
+    def train(self):
+        for i in range(trainning_loop):
+            batch_mask = np.random.choice(train_size, batch_size)
 
-#Pre definition
-ipsize = 784 #used for MNIST
-hlist = [100, 100, 100, 100, 100, 100]
-opsize = 10
+            x_batch = x_train[batch_mask]
+            t_batch = t_train[batch_mask]
 
-class SGD:
-    def __init__(self, lrate = 0.01):
-        self.lrate = lrate
-    '''
-    parameter is a dictionary,
-    grads is a result array, shall be calculated before call the function
-    grads[] contain dl/dp
-    '''
-    def updatenetwork(self, parameters, grads):
-        for key in parameters.keys():
-            parameters -= self.lrate * grads[key]
+            '''
+            Calculate grad:
+            {
+            x_batch: trainning set
+            t_batch: test set
+            }
+            '''
+            grads = networktype.gradient(x_batch, t_batch)
+            if optimizer == 'SGD':
+                optimizer.update_network(networktype.params, grads)
+            else:
+                raise Exception
 
-#Initilize network
-network = MultiLayerNet(input_size=ipsize, hidden_size_list=hlist, output_size=opsize)
-optimizer = SGD()
+            iter_per_epoch = max(self.train_size / mini_batch_size, 1)
+            if i % iter_per_epoch == 0:
+                train_acc = network.accuracy(x_train, t_train)
+                test_acc = network.accuracy(x_test, t_test)
+                train_accurate_list.append(train_acc)
+                test_accurate_list.append(test_acc)
 
-(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
-#Validation for over fitting
-x_train = x_train[:300]
-t_train = t_train[:300]
-
-
-'''
-Define neural network parameters
-'''
-max_epochs = 200
-train_size = x_train.shape[0]
-batch_size = 100
-learning_step = 0
-
-train_loss_list = []
-train_accurate_list = []
-test_accurate_list = []
-
-'''
-iter_per_epoch: complete one trainning
-'''
-iter_per_epoch = max(train_size/batch_size, 1)
-epoch_count = 0
-
-for i in range(learning_step):
-    batch_mask = np.random.choice(train_size, batch_size)
-
-    x_batch = x_train[batch_mask]
-    t_batch = t_train[batch_mask]
-
-    '''
-    Calculate grad:
-    {
-    x_batch: trainning set
-    t_batch: test set
-    }
-    '''
-    grads = network.gradient(x_batch, t_batch)
-    optimizer.update(network.params, grads)
-
-    if i % iter_per_epoch == 0:
-        train_acc = network.accuracy(x_train, t_train)
-        test_acc = network.accuracy(x_test, t_test)
-        train_accurate_list.append(train_acc)
-        test_accurate_list.append(test_acc)
-
-        epoch_count += 1
-        if epoch_count >= max_epochs:
-            break
-
-class DrawFigure:
-    def __init__(self):
-        plt.title('Over fitting Chart')
-        self.fig = plt.figure(1)
-    def setXrange(self,xmin=None,xmax=None):
-        if None == xmin or None == xmax:
-            pass
-        else:
-            plt.xlim(xmin, xmax)
-    def setYrange(self,ymin=None,ymax=None):
-        if None == ymin or None == ymax:
-            pass
-        else:
-            plt.ylim(ymin, ymax)
-    def plotchart(self,x,y,position):
-        self.fig.add_subplot(position[0], position[1], position[2])
-        plt.plot(x,y)
-    def plotshow(self):
-        plt.show()
-
-df = DrawFigure()
-epoch =
-df.plotchart(epoch_count, train_accurate_list, [1,2,1])
-df.plotchart(epoch_count, test_accurate_list, [1,2,2])
-
+                epoch_count += 1
+                if epoch_count >= max_epochs:
+                    break
